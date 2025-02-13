b9eeb550a3ffb7ffcf1f2c43b16303a39d495a91
Dani <danihervas98@gmail.com>
2025-02-13 13:12:39 +0100

    header arreglado

--- app/assets/components/headerComponent.tsx
+++ app/assets/components/headerComponent.tsx
@@ -5,17 +5,15 @@ import SearchBar from "./searchBarComponent";
 const Header: React.FC = () => {
   return (
     <header className="bg-black py-4 px-6 flex items-center justify-between">
-    
       <div className="flex-shrink-0">
         <img src="/logo.png" alt="Logo" className="w-12 h-12 rounded-full" />
       </div>
 
-      
+      {/* Contenedor del SearchBar sin el fondo blanco */}
       <div className="flex-grow max-w-md mx-4">
         <SearchBar />
       </div>
 
-     
       <div className="flex-shrink-0">
         <Navbar />
       </div>
--- app/assets/components/searchBarComponent.tsx
+++ app/assets/components/searchBarComponent.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"; 
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const SearchBar: React.FC = () => {
   return (
-    <div className="relative w-full max-w-md my-5">
+    <div className="relative w-full max-w-md my-0">
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" />
       <input
         type="text"
         placeholder="Search"
-        className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full pl-10 pr-4 py-2 border border-gray-300 bg-white text-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
   );
 };
 
 export default SearchBar;
+
